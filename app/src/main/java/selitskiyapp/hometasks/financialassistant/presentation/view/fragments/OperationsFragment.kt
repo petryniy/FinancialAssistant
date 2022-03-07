efa9dc7ea69a41d9120d1451fdcb09d2dabb9ffc
petryniy <selitskiy.anton1@gmail.com>
2022-03-08 00:28:15 +0300

    add headView date to OperationsFragment.kt, fix filter destination

--- app/src/main/java/selitskiyapp/hometasks/financialassistant/presentation/view/fragments/OperationsFragment.kt
+++ app/src/main/java/selitskiyapp/hometasks/financialassistant/presentation/view/fragments/OperationsFragment.kt
@@ -1,21 +1,22 @@
 package selitskiyapp.hometasks.financialassistant.presentation.view.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.zip
 import selitskiyapp.hometasks.financialassistant.R
 import selitskiyapp.hometasks.financialassistant.databinding.FragmentOperationsBinding
+import selitskiyapp.hometasks.financialassistant.domain.models.BaseItem
+import selitskiyapp.hometasks.financialassistant.domain.models.HeadItem
 import selitskiyapp.hometasks.financialassistant.domain.models.OperationWithMoneyHolder
 import selitskiyapp.hometasks.financialassistant.presentation.recyclers.operations.OperationsAdapter
 import selitskiyapp.hometasks.financialassistant.presentation.recyclers.operations.OperationsOnItemListener
@@ -48,21 +49,28 @@ class OperationsFragment : Fragment(R.layout.fragment_operations) {
 
         initAddButton()
 
+        initRecycler()
+
         initObservers()
 
-        initRecycler()
+//        initRecyclerAdapter(listOperations)
+
+    }
+
+    private fun initRecyclerAdapter(listOperations: List<OperationWithMoneyHolder?>) {
+        listBaseItem = listOperations.toMutableList()
+        listBaseItem.add(0, HeadItem("22.02.2022"))
+        adapter.submitList(listBaseItem)
     }
 
     private fun initObservers() {
         lifecycleScope.launchWhenResumed {
             sharedViewModel.filter.collect { newFilter ->
-                viewModel.viewModelScope.launch {
-                    viewModel.getFilteredOperationsListFlow(newFilter).collect {
-                       listOperations = it.sortedByDescending { operationWithMoneyHolder ->
-                           operationWithMoneyHolder?.operationEntity?.date
-                        }
-                        adapter.submitList(listOperations)
+                viewModel.getFilteredOperationsListFlow(newFilter).collect {
+                    listOperations = it.sortedByDescending { operationWithMoneyHolder ->
+                        operationWithMoneyHolder?.operationEntity?.date
                     }
+                    initRecyclerAdapter(listOperations)
                 }
             }
         }
@@ -83,6 +91,11 @@ class OperationsFragment : Fragment(R.layout.fragment_operations) {
     companion object {
         const val ID_FROM_OPERATIONS_FRAGMENT = "OPERATIONS_FRAGMENT"
         private var listOperations: List<OperationWithMoneyHolder?> = emptyList()
+        private var listBaseItem: MutableList<BaseItem?> = emptyList<BaseItem>().toMutableList()
+//        = listOf(OperationWithMoneyHolder(
+//            OperationEntity(3, "2", 3, 4, 2131165310, "6", "7"),
+//        MoneyHolderEntity(1,"2", 2131165294,4)
+//        ))
     }
 }
 
